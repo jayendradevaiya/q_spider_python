4055a252c1c6b7446c30d0f1ad4ddec0907fcae3
jayendradevaiya <161676471+jayendradevaiya@users.noreply.github.com>
2026-01-07 17:35:59 +0530

    Add files via upload

---
+++ 01_besic/01_besic.py
@@ -0,0 +1,34 @@
+#  Day-1
+# a=10
+# b=2
+# print(a+b,end=' ',sep='$')
+
+
+## write a programme to perform arith matic operations on the user input
+# a= int(input("Enter the number:"))
+# b= int(input("Enter the number:"))
+
+# print(f"{a} + {b} = {a+b}")
+# print(f"{a} - {b} = {a-b}")
+# print(f"{a} * {b} = {a*b}")
+# print(f"{a} / {b} = {a/b}")
+# print(f"{a} // {b} = {a//b}")
+# print(f"{a} ** {b} = {a**b}")
+# print(f"{a} % {b} = {a%b}")
+
+
+#  Day -2
+
+# a= eval(input("Enter the number:"))
+
+# print(a,type(a))
+# print(f"type of data is {a} and datatype {type(a)}")
+
+
+#  write a program to check wether the given number is even or odd
+
+a= int(input("Enter the number:"))
+# print(f"{a%2==0}")
+if a %2 == 0:
+    print("even number")
+print("odd num")
---
+++ 02_controller/01_if.py
@@ -0,0 +1,6 @@
+# 1. write a program to check wether the given number is even or odd
+
+a= int(input("Enter the number:"))
+
+if a %2 == 0:
+    print("even number")
